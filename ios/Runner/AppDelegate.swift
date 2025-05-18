683cce8fae6f79372a5734a48ff359d34fd507ee
Sh4y0 <Sh4y0>
2025-05-18 13:12:48 -0500

    Agregando mapa y geolocalizacion

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyAbhFG1G_twkuFENtlvHzAGFXFiQUvp0hs")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
