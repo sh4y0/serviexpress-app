204335fe59e3efa50f4255f39556eb2525cf1717
elAsksito <xpheri035@gmail.com>
2025-06-19 01:26:03 -0500

    refactor: cambiando el setState a NotificationValue

--- lib/presentation/pages/chat_page.dart
+++ lib/presentation/pages/chat_page.dart
@@ -18,9 +18,10 @@ class ChatScreen extends ConsumerStatefulWidget {
 }
 
 class _ChatScreenState extends ConsumerState<ChatScreen> {
-  String? userId;
+  final ValueNotifier<String?> _userId = ValueNotifier<String?>(null);
+
   final clienteId = "0eGwG6nvcTfneiTvVSSBI3G9HV53";
-  bool _errorShown = false;
+  final ValueNotifier<bool> _errorShown = ValueNotifier<bool>(false);
 
   late final String chatUid;
 
@@ -41,6 +42,8 @@ class _ChatScreenState extends ConsumerState<ChatScreen> {
     super.dispose();
     _messageController.dispose();
     _scrollController.dispose();
+    _userId.dispose();
+    _errorShown.dispose();
   }
 
   String getSortedChatUid(String uid1, String uid2) {
@@ -56,14 +59,12 @@ class _ChatScreenState extends ConsumerState<ChatScreen> {
 
       LoadingScreen.hide();
 
-      setState(() {
-        userId = id;
-      });
+      _userId.value = id;
 
-      if (id == null && !_errorShown) {
+      if (id == null && !_errorShown.value) {
         WidgetsBinding.instance.addPostFrameCallback((_) {
-          if (!_errorShown) {
-            _errorShown = true;
+          if (!_errorShown.value) {
+            _errorShown.value = true;
             Alerts.instance.showErrorAlert(
               context,
               "No se pudo obtener el ID del usuario.",
@@ -73,9 +74,8 @@ class _ChatScreenState extends ConsumerState<ChatScreen> {
       }
     } catch (e) {
       LoadingScreen.hide();
-      setState(() {});
-      if (!_errorShown) {
-        _errorShown = true;
+      if (!_errorShown.value) {
+        _errorShown.value = true;
         Alerts.instance.showErrorAlert(
           context,
           "Ocurrió un error al obtener el ID: $e",
@@ -86,101 +86,103 @@ class _ChatScreenState extends ConsumerState<ChatScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final getMessages = ref.watch(
-      chatMessagesProvider(getSortedChatUid(userId!, clienteId)),
-    );
-
-    ref.listen<ResultState>(chatViewModelProvider, (previous, next) {
-      switch (next) {
-        case Idle():
-          LoadingScreen.hide();
-          break;
-        case Loading():
-          break;
-        case Success():
-          LoadingScreen.hide();
-          _messageController.clear();
-          break;
-        case Failure(:final error):
-          LoadingScreen.hide();
-          if (mounted) {
-            Alerts.instance.showErrorAlert(context, error.message);
+    return ValueListenableBuilder<String?>(
+      valueListenable: _userId,
+      builder: (context, userId, _) {
+        final getMessages = ref.watch(
+          chatMessagesProvider(getSortedChatUid(userId!, clienteId)),
+        );
+        ref.listen<ResultState>(chatViewModelProvider, (previous, next) {
+          switch (next) {
+            case Idle():
+              LoadingScreen.hide();
+              break;
+            case Loading():
+              break;
+            case Success():
+              LoadingScreen.hide();
+              _messageController.clear();
+              break;
+            case Failure(:final error):
+              LoadingScreen.hide();
+              if (mounted) {
+                Alerts.instance.showErrorAlert(context, error.message);
+              }
+              break;
           }
-          break;
-      }
-    });
-
-    return Scaffold(
-      backgroundColor: AppColor.bgChat,
-      appBar: AppBar(
-        backgroundColor: AppColor.bgChat,
-        leading: IconButton(
-          onPressed: () {
-            Navigator.pop(context, AppRoutes.login);
-          },
-          icon: Transform.translate(
-            offset: const Offset(4, 0),
-            child: const Icon(Icons.arrow_back_ios, color: Colors.white),
-          ),
-          style: IconButton.styleFrom(backgroundColor: AppColor.bgBack),
-        ),
-        title: const Text(
-          "Fedor Kiryakov",
-          style: TextStyle(color: Colors.white, fontSize: 17),
-        ),
-        centerTitle: true,
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: SvgPicture.asset(
-              "assets/icons/ic_person.svg",
-              width: 30,
-              height: 30,
-            ),
-          ),
-        ],
-      ),
-      body: SafeArea(
-        child: Column(
-          children: [
-            const SizedBox(height: 10),
-            Expanded(
-              child: getMessages.when(
-                data: (messages) {
-                  WidgetsBinding.instance.addPostFrameCallback((_) {
-                    if (_scrollController.hasClients) {
-                      _scrollController.animateTo(
-                        _scrollController.position.maxScrollExtent,
-                        duration: const Duration(milliseconds: 300),
-                        curve: Curves.easeOut,
-                      );
-                    }
-                  });
-                  return ListView.builder(
-                    controller: _scrollController,
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    itemCount: messages.length,
-                    itemBuilder: (context, index) {
-                      final message = messages[index];
-                      final isUser = message.senderId == userId;
-                      return isUser
-                          ? _userMessage(message.content)
-                          : _clientMessage(message.content);
-                    },
-                  );
-                },
-                loading: () => const Center(child: CircularProgressIndicator()),
-                error: (error, stack) {
-                  return const Center(
-                    child: Text(
-                      "Error al cargar los mensajes",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  );
-                },
+        });
+        return Scaffold(
+          backgroundColor: AppColor.bgChat,
+          appBar: AppBar(
+            backgroundColor: AppColor.bgChat,
+            leading: IconButton(
+              onPressed: () {
+                Navigator.pop(context, AppRoutes.login);
+              },
+              icon: Transform.translate(
+                offset: const Offset(4, 0),
+                child: const Icon(Icons.arrow_back_ios, color: Colors.white),
               ),
+              style: IconButton.styleFrom(backgroundColor: AppColor.bgBack),
+            ),
+            title: const Text(
+              "Fedor Kiryakov",
+              style: TextStyle(color: Colors.white, fontSize: 17),
+            ),
+            centerTitle: true,
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: SvgPicture.asset(
+                  "assets/icons/ic_person.svg",
+                  width: 30,
+                  height: 30,
+                ),
+              ),
+            ],
+          ),
+          body: SafeArea(
+            child: Column(
+              children: [
+                const SizedBox(height: 10),
+                Expanded(
+                  child: getMessages.when(
+                    data: (messages) {
+                      WidgetsBinding.instance.addPostFrameCallback((_) {
+                        if (_scrollController.hasClients) {
+                          _scrollController.animateTo(
+                            _scrollController.position.maxScrollExtent,
+                            duration: const Duration(milliseconds: 300),
+                            curve: Curves.easeOut,
+                          );
+                        }
+                      });
+                      return ListView.builder(
+                        controller: _scrollController,
+                        padding: const EdgeInsets.symmetric(horizontal: 16),
+                        itemCount: messages.length,
+                        itemBuilder: (context, index) {
+                          final message = messages[index];
+                          final isUser = message.senderId == userId;
+                          return isUser
+                              ? _userMessage(message.content)
+                              : _clientMessage(message.content);
+                        },
+                      );
+                    },
+                    loading:
+                        () => const Center(child: CircularProgressIndicator()),
+                    error: (error, stack) {
+                      return const Center(
+                        child: Text(
+                          "Error al cargar los mensajes",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      );
+                    },
+                  ),
 
-              /*child: ListView(
+                  /*child: ListView(
                 padding: const EdgeInsets.symmetric(horizontal: 16),
                 children: [
                   _userMessage("Hi"),
@@ -235,100 +237,105 @@ class _ChatScreenState extends ConsumerState<ChatScreen> {
                   ),
                 ],
               ),*/
-            ),
-            const SizedBox(height: 15),
-
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 15),
-              decoration: BoxDecoration(
-                color: AppColor.bgContendeor,
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(40),
-                  topRight: Radius.circular(40),
                 ),
-                boxShadow: [
-                  BoxShadow(
-                    color: AppColor.bgShadow.withAlpha((0.3 * 255).toInt()),
-                    offset: const Offset(0, -4),
-                    blurRadius: 6,
-                    spreadRadius: 0.1,
+                const SizedBox(height: 15),
+
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 18,
+                    vertical: 15,
                   ),
-                ],
-              ),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      controller: _messageController,
-                      maxLines: null,
-                      minLines: 1,
-                      style: const TextStyle(color: AppColor.txtMsg),
-                      decoration: InputDecoration(
-                        hintText: "Tu mensaje aquí",
-                        hintStyle: const TextStyle(
-                          color: AppColor.txtMsg,
-                          fontSize: 17,
-                        ),
-                        filled: true,
-                        fillColor: AppColor.bgLabel,
-                        contentPadding: const EdgeInsets.symmetric(
-                          horizontal: 12,
-                          vertical: 18,
-                        ),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                          borderSide: BorderSide.none,
-                        ),
-                        suffixIcon: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            IconButton(
-                              onPressed: () {},
-                              icon: SvgPicture.asset(
-                                "assets/icons/ic_camara.svg",
-                                width: 30,
-                                height: 30,
-                              ),
+                  decoration: BoxDecoration(
+                    color: AppColor.bgContendeor,
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(40),
+                      topRight: Radius.circular(40),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: AppColor.bgShadow.withAlpha((0.3 * 255).toInt()),
+                        offset: const Offset(0, -4),
+                        blurRadius: 6,
+                        spreadRadius: 0.1,
+                      ),
+                    ],
+                  ),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          controller: _messageController,
+                          maxLines: null,
+                          minLines: 1,
+                          style: const TextStyle(color: AppColor.txtMsg),
+                          decoration: InputDecoration(
+                            hintText: "Tu mensaje aquí",
+                            hintStyle: const TextStyle(
+                              color: AppColor.txtMsg,
+                              fontSize: 17,
                             ),
-                            IconButton(
-                              onPressed: () async {
-                                final message = _messageController.text;
-                                if (message.isEmpty) {
-                                  Alerts.instance.showErrorAlert(
-                                    context,
-                                    "Escribe un mensaje.",
-                                  );
-                                  return;
-                                }
-
-                                final messageModel = MessageModel(
-                                  senderId: userId!,
-                                  receiverId: clienteId,
-                                  content: message,
-                                  timestamp: DateTime.now(),
-                                );
-
-                                await ref
-                                    .read(chatViewModelProvider.notifier)
-                                    .sendMessage(messageModel);
-                              },
-                              icon: SvgPicture.asset(
-                                "assets/icons/ic_enviar.svg",
-                                width: 30,
-                                height: 30,
-                              ),
+                            filled: true,
+                            fillColor: AppColor.bgLabel,
+                            contentPadding: const EdgeInsets.symmetric(
+                              horizontal: 12,
+                              vertical: 18,
                             ),
-                          ],
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                              borderSide: BorderSide.none,
+                            ),
+                            suffixIcon: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                IconButton(
+                                  onPressed: () {},
+                                  icon: SvgPicture.asset(
+                                    "assets/icons/ic_camara.svg",
+                                    width: 30,
+                                    height: 30,
+                                  ),
+                                ),
+                                IconButton(
+                                  onPressed: () async {
+                                    final message = _messageController.text;
+                                    if (message.isEmpty) {
+                                      Alerts.instance.showErrorAlert(
+                                        context,
+                                        "Escribe un mensaje.",
+                                      );
+                                      return;
+                                    }
+
+                                    final messageModel = MessageModel(
+                                      senderId: userId,
+                                      receiverId: clienteId,
+                                      content: message,
+                                      timestamp: DateTime.now(),
+                                    );
+
+                                    await ref
+                                        .read(chatViewModelProvider.notifier)
+                                        .sendMessage(messageModel);
+                                  },
+                                  icon: SvgPicture.asset(
+                                    "assets/icons/ic_enviar.svg",
+                                    width: 30,
+                                    height: 30,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
                         ),
                       ),
-                    ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
