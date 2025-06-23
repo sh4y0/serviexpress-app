43965c356509ee90a9e463bd32c7d7dd2960538b
Sh4y0 <Sh4y0>
2025-06-22 21:52:28 -0500

    añadir nuevos iconos de categoría, actualizar el mock de categoría e integrar el servicio Google Maps

---
+++ lib/data/service/google_maps_service.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:http/http.dart' as http;
+import 'package:serviexpress_app/data/service/remote_config_service.dart';
+
+class GoogleMapsService {
+  final String _apiKey = RemoteConfigService.instance.getGoogleMapsKey();
+
+  Future<List<LatLng>> getDirections(LatLng origin, LatLng destination) async {
+    final String url =
+        'https://maps.googleapis.com/maps/api/directions/json?origin=${origin.latitude},${origin.longitude}&destination=${destination.latitude},${destination.longitude}&key=$_apiKey';
+
+    try {
+      final response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        final jsonResponse = json.decode(response.body);
+
+        if ((jsonResponse['routes'] as List).isNotEmpty) {
+          final String encodedPolyline =
+              jsonResponse['routes'][0]['overview_polyline']['points'];
+
+          final polylinePoints = PolylinePoints();
+          final List<PointLatLng> decodedPoints = polylinePoints.decodePolyline(
+            encodedPolyline,
+          );
+          return decodedPoints
+              .map((point) => LatLng(point.latitude, point.longitude))
+              .toList();
+        } else {
+          debugPrint('No se encontraron rutas: ${jsonResponse['status']}');
+          return [];
+        }
+      } else {
+        debugPrint('Error en la API de Direcciones: ${response.statusCode}');
+        debugPrint('Respuesta: ${response.body}');
+        return [];
+      }
+    } catch (e) {
+      debugPrint('Excepción al obtener direcciones: $e');
+      return [];
+    }
+  }
+}
